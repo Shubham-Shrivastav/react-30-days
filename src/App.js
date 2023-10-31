94e3d9dc617046a28cb59c61b4abc81f6a486929
Shubham Shrivastav <shuboom969@gmail.com>
2023-10-31 17:08:55 +0530

    Add simple about componet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import React, { useState } from 'react';
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
 
 function App() {
+  const [page, setPage] = useState('home');
+
+  const navigateTo = (pageName) => {
+    setPage(pageName);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-    Hello World !
-      </header>
+    <div className='App'>
+        <nav>
+          <ul>
+          <li onClick={() => navigateTo('home')}>Home</li>
+          <li onClick={() => navigateTo('about')}>About Us</li>
+          </ul>
+        </nav>
+      <main>
+        {page === 'home' && <Home />}
+        {page === 'about' && <About />}
+      </main>
     </div>
   );
 }
