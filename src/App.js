0c6cf63c5a4036737de0b44dc393b6412436f5e2
Shubham Shrivastav <shuboom969@gmail.com>
2023-11-03 01:40:08 +0530

    Add header and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import Navbar from './components/Navbar';
-
+import Header from './components/Header';
+import Footer from './components/Footer'
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <Navbar />
+        <Header />
       </header>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className='footer-content'>
+        <p>&copy; 2023 | Shubham Shrivastav |</p>
+        <ul>
+          <li>
+            <a href='#privacy'>Privacy Policy</a>
+          </li>
+          <li>
+            <a href='#terms'>Terms of Service</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
