e5287764f23802edc691e4c2a56d50fe0b12c19c
Shubham Shrivastav <shuboom969@gmail.com>
2023-11-06 20:59:04 +0530

    Add ESLint config

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+function App () {
   return (
     <div className="App">
       <header className="App-header">
     Hello World !
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
