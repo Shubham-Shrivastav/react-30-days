f77a556abb3bee028c970b6c0fa8b5447e9b020c
Shubham Shrivastav <shuboom969@gmail.com>
2023-11-07 14:59:26 +0530

    Add API that shows the Dog breeds with async/awat

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './App.css'
-import Counter from './components/Counter'
+import DataFetcher from './components/DataFetcher'
 function App () {
   return (
     <>
     <div className='App'>
       <header className='App-header'>
-        <Counter />
+        <DataFetcher />
       </header>
     </div>
     </>
---
+++ src/components/DataFetcher.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+
+const DataFetcher = () => {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function fetchData () {
+      try {
+        const response = await fetch('https://dog.ceo/api/breeds/list/all')
+        const result = await response.json()
+        if (result.status === 'success') {
+          setData(result.message)
+        } else {
+          console.error('Error fetching data:', result.message)
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+      setLoading(false)
+    }
+
+    fetchData()
+  }, [])
+
+  return (
+    <>
+      <h2>Data from Public API that conclude all the dogs breeds</h2>
+      {loading && <p>Loading Data ...</p>}
+      {!loading && (
+        <ul>
+          {Object.keys(data).map((breed, index) => (
+            <li key={index}>{breed}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  )
+}
+
+export default DataFetcher
