394fc2bd50ffa426002402ecef3e9e23f6a23e3a
Shubham Shrivastav <shuboom969@gmail.com>
2023-11-08 18:47:28 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Dashboard from './components/Dashboard'
 
 function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-    Hello World !
-      </header>
+    <div className='App'>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="about" element={<About />}/>
+          <Route path="contact" element={<Contact />}/>
+          <Route path="dashboard" element={<Dashboard />}/>
+        </Routes>
     </div>
   )
 }
