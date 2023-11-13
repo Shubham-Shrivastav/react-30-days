2c9ada4a2227d2039663d92fefa4ae2fae172d63
Shubham Shrivastav <shuboom969@gmail.com>
2023-11-14 01:19:10 +0530

    Setup formik

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,19 @@
-import { Stack } from '@mui/material'
-import { Form } from 'formik'
+import { Button, Stack } from '@mui/material'
+import { Field, Form, Formik } from 'formik'
 import React from 'react'
 
 const Login = () => {
     return (
         <Stack>
-            <Form>
-                <input>Hello</input>
-            </Form>
+            <Formik initialValues={{ email: '', password: '' }} onSubmit={(values) => {
+                console.log(values);
+            }}>
+                <Form>
+                    <Field name='email' />
+                    <Field name='password' />
+                    <Button type='submit'>Login</Button>
+                </Form>
+            </Formik>
         </Stack>
     )
 }
