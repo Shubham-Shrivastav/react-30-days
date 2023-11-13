af3a1769131c0358e219dcdcc2ae807fa61046ba
Shubham Shrivastav <shuboom969@gmail.com>
2023-11-14 02:10:54 +0530

    Add a bit style connect Formik with MUI

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,21 +1,40 @@
-import { Button, Stack } from '@mui/material'
-import { Field, Form, Formik } from 'formik'
-import React from 'react'
+import { Button, Container, Stack } from '@mui/material';
+import { Form, Formik, Field } from 'formik';
+import { TextField } from 'formik-mui';
 
 const Login = () => {
     return (
-        <Stack>
-            <Formik initialValues={{ email: '', password: '' }} onSubmit={(values) => {
-                console.log(values);
-            }}>
-                <Form>
-                    <Field name='email' />
-                    <Field name='password' />
-                    <Button type='submit'>Login</Button>
-                </Form>
-            </Formik>
-        </Stack>
-    )
-}
+        <Container sx={{ display: 'flex', height: '100vh', alignItems: 'center', justifyContent: 'center' }}>
+            <Stack sx={{ width: '270px' }}>
+                <Formik
+                    initialValues={{ email: '', password: '' }}
+                    onSubmit={(values) => {
+                        console.log('Form Values:', values);
+                    }}
+                >
+                    <Form>
+                        <Field
+                            component={TextField}
+                            id="outlined-basic-email"
+                            label="Email"
+                            variant="outlined"
+                            name="email"
+                            type='email'
+                        />
+                        <Field
+                            component={TextField}
+                            id="outlined-basic-password"
+                            label="Password"
+                            variant="outlined"
+                            name="password"
+                            type="password"
+                        />
+                        <Button type="submit">Login</Button>
+                    </Form>
+                </Formik>
+            </Stack>
+        </Container>
+    );
+};
 
-export default Login
+export default Login;
--- src/components/SignUp/Signup.jsx
+++ src/components/SignUp/Signup.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { Button, Container, Stack } from '@mui/material';
+import { Form, Formik, Field } from 'formik';
+import { TextField } from 'formik-mui';
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+    return (
+        <Container sx={{ display: 'flex', height: '100vh', alignItems: 'center', justifyContent: 'center' }}>
+            <Stack sx={{ width: '270px' }}>
+                <Formik
+                    initialValues={{ email: '', password: '', name: '' }}
+                    onSubmit={(values) => {
+                        console.log('Form Values:', values);
+                    }}
+                >
+                    <Form>
+                        <Field
+                            component={TextField}
+                            id="outlined-basic-email"
+                            label="Name"
+                            variant="outlined"
+                            name="name"
+                            placeholder="Enter Name.."
+                        />
+                        <Field
+                            component={TextField}
+                            id="outlined-basic-email"
+                            label="Email"
+                            variant="outlined"
+                            name="email"
+                            type='email'
+                            placeholder="Enter Email.."
 
-export default Signup
+                        />
+                        <Field
+                            component={TextField}
+                            id="outlined-basic-password"
+                            label="Password"
+                            variant="outlined"
+                            name="password"
+                            type="password"
+                            placeholder="Enter Password.."
+
+                        />
+                        <Button type="submit">Sign-Up</Button>
+                    </Form>
+                </Formik>
+            </Stack>
+        </Container>
+    );
+};
+
+export default Signup;
