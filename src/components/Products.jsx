f9f50bbdd5ad23334a2ed7fd4d5e75af0fa23481
Shubham Shrivastav <shuboom969@gmail.com>
2023-11-16 17:02:20 +0530

    Add list items and cleanUp, create a seperate comp

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
+import { bucket } from './ListData'
 
-const bucket = [
-  { name: 'Apple', isFruit: true, id: 1 },
-  { name: 'Banana', isFruit: true, id: 2 },
-  { name: 'Potato', isFruit: false, id: 3 },
-  { name: 'Kiwi', isFruit: true, id: 4 },
-  { name: 'cabbage', isFruit: false, id: 5 },
-  { name: 'garlic', isFruit: false, id: 6 }
-]
-
-export default function Products () {
+export default function Products() {
   const listItems = bucket.map(bucket => (
-    <li key={ bucket.id } style={{ color: bucket.isFruit ? 'purple' : 'green' }}>
+    <li key={bucket.id} style={{ color: bucket.isFruit ? 'purple' : 'green' }}>
       {bucket.name}
     </li>
   ))
