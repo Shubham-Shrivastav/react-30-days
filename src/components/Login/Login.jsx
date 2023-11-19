d893f4be673656ba506189762b760ae66d65287c
Shubham Shrivastav <shuboom969@gmail.com>
2023-11-19 19:52:06 +0530

    Add simple validation

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,44 +1,61 @@
-import { Button, Container, Stack } from '@mui/material'
-import { Form, Formik, Field } from 'formik'
-import { TextField } from 'formik-mui'
+import React from 'react';
+import { Button, Container, Stack } from '@mui/material';
+import { Form, Formik, Field } from 'formik';
+import { TextField } from 'formik-mui';
 
 const Login = () => {
-    return (
-        <Container sx={{ display: 'flex', height: 'auto', alignItems: 'center', justifyContent: 'center' }}>
-            <Stack elevation={4} sx={{ width: '300px', boxShadow: "12", height: '320px' }}>
-                <Formik
-                    initialValues={{ email: '', password: '' }}
-                    onSubmit={(values) => {
-                        console.log('Form Values:', values);
-                    }}
-                >
-                    <Form
-                    ><p>Login</p>
-                        <Field
-                            component={TextField}
-                            autoComplete="off"
-                            id="outlined-basic-email"
-                            label="Email"
-                            variant="outlined"
-                            name="email"
-                            type='email'
-                            sx={{ m: 2 }} />
-                        <Field
-                            component={TextField}
-                            autoComplete="off"
-                            id="outlined-basic-password"
-                            label="Password"
-                            variant="outlined"
-                            name="password"
-                            type="password"
-                            sx={{ m: 2 }} />
-                        <Button variant="contained"
-                            type="submit">Login</Button>
-                    </Form>
-                </Formik>
-            </Stack>
-        </Container>
-    );
+  return (
+    <Container sx={{ display: 'flex', height: 'auto', alignItems: 'center', justifyContent: 'center' }}>
+      <Stack elevation={4} sx={{ width: '300px', boxShadow: '12', height: '320px' }}>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          onSubmit={(values, { setSubmitting }) => {
+            console.log('Form Values:-----', values);
+            setSubmitting(false); 
+          }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.email) {
+              errors.email = 'Email is required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+              errors.email = 'Invalid email address';
+            }
+            if (!values.password) {
+              errors.password = 'Password is required';
+            }
+            return errors;
+          }}
+        >
+          <Form>
+            <p>Login</p>
+            <Field
+              component={TextField}
+              autoComplete="off"
+              id="outlined-basic-email"
+              label="Email"
+              variant="outlined"
+              name="email"
+              type="email"
+              sx={{ m: 2 }}
+            />
+            <Field
+              component={TextField}
+              autoComplete="off"
+              id="outlined-basic-password"
+              label="Password"
+              variant="outlined"
+              name="password"
+              type="password"
+              sx={{ m: 2 }}
+            />
+            <Button variant="contained" type="submit">
+              Login
+            </Button>
+          </Form>
+        </Formik>
+      </Stack>
+    </Container>
+  );
 };
 
 export default Login;
--- src/components/SignUp/Signup.jsx
+++ src/components/SignUp/Signup.jsx
@@ -1,27 +1,46 @@
-import { Button, Container, Stack } from '@mui/material'
-import { Form, Formik, Field } from 'formik'
-import { TextField } from 'formik-mui'
+import React from 'react';
+import { Button, Container, Stack } from '@mui/material';
+import { Form, Formik, Field } from 'formik';
+import { TextField } from 'formik-mui';
 
 const Signup = () => {
     return (
         <Container sx={{ display: 'flex', height: '100vh', alignItems: 'center', justifyContent: 'center' }}>
-            <Stack elevation={4} sx={{ width: '300px', boxShadow: "12", height: '420px' }}>
+            <Stack elevation={4} sx={{ width: '300px', boxShadow: '12', height: '420px' }}>
                 <Formik
                     initialValues={{ email: '', password: '', name: '' }}
-                    onSubmit={(values) => {
-                        console.log('Form Values:', values)
-                    }} >
-                    <Form >
-                        <p>Sign-UP</p>
+                    onSubmit={(values, { setSubmitting }) => {
+                        console.log('Form Values--:', values);
+                        setSubmitting(false);
+                    }}
+                    validate={(values) => {
+                        const errors = {};
+                        if (!values.name) {
+                            errors.name = 'Name is required';
+                        }
+                        if (!values.email) {
+                            errors.email = 'Email is required';
+                        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                            errors.email = 'Invalid email address';
+                        }
+                        if (!values.password) {
+                            errors.password = 'Password is required';
+                        }
+                        return errors;
+                    }}
+                >
+                    <Form>
+                        <p>Sign Up</p>
                         <Field
                             component={TextField}
                             autoComplete="off"
-                            id="outlined-basic-email"
+                            id="outlined-basic-name"
                             label="Name"
                             variant="outlined"
                             name="name"
                             placeholder="Enter Name.."
-                            sx={{ m: 2 }} />
+                            sx={{ m: 2 }}
+                        />
                         <Field
                             component={TextField}
                             autoComplete="off"
@@ -29,9 +48,10 @@ const Signup = () => {
                             label="Email"
                             variant="outlined"
                             name="email"
-                            type='email'
+                            type="email"
                             placeholder="Enter Email.."
-                            sx={{ m: 2 }} />
+                            sx={{ m: 2 }}
+                        />
                         <Field
                             component={TextField}
                             id="outlined-basic-password"
@@ -40,13 +60,16 @@ const Signup = () => {
                             name="password"
                             type="password"
                             placeholder="Enter Password.."
-                            sx={{ m: 2 }} />
-                        <Button type="submit" variant="outlined">Sign-Up</Button>
+                            sx={{ m: 2 }}
+                        />
+                        <Button type="submit" variant="outlined">
+                            Sign Up
+                        </Button>
                     </Form>
                 </Formik>
             </Stack>
         </Container>
-    )
-}
+    );
+};
 
-export default Signup
+export default Signup;
