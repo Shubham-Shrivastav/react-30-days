1196cb7cd52ee99dec3fd401d7cb50950743169a
Shubham Shrivastav <shuboom969@gmail.com>
2023-11-19 15:45:43 +0530

    Add a simple Toggle button with MUI styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import { Button, Stack } from '@mui/material';
 
-function App () {
+function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-    Hello World !
-      </header>
-    </div>
-  )
+    <>
+      <div className={`App ${isDarkMode ? 'darkMode' : 'lightMode'}`}>
+        <Stack direction="row">
+          <Button onClick={toggleDarkMode}>
+            {isDarkMode ? <LightModeIcon /> : <DarkModeIcon />}
+          </Button>
+        </Stack>
+        <header className="App-header">
+          Hello World!
+        </header>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
