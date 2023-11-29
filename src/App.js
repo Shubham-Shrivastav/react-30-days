985a4f8b1084eca1ccdd303bf9cba235a2624cc3
Shubham Shrivastav <shuboom969@gmail.com>
2023-11-29 22:24:51 +0530

    Day 29-30

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import { Button, Stack } from '@mui/material';
 
+const MemoizedButton = React.memo(({ onClick, isDarkMode }) => (
+  <Button onClick={onClick}>
+    {isDarkMode ? <LightModeIcon /> : <DarkModeIcon />}
+  </Button>
+));
+
 function App() {
   const storedDarkMode = JSON.parse(localStorage.getItem('darkMode'));
   const [isDarkMode, setIsDarkMode] = useState(storedDarkMode || false);
@@ -20,9 +26,7 @@ function App() {
     <>
       <div className={`App ${isDarkMode ? 'darkMode' : 'lightMode'}`}>
         <Stack direction="row">
-          <Button onClick={toggleDarkMode}>
-            {isDarkMode ? <LightModeIcon /> : <DarkModeIcon />}
-          </Button>
+          <MemoizedButton onClick={toggleDarkMode} isDarkMode={isDarkMode} />
         </Stack>
         <header className="App-header">
           Hello World!
